9f9e868487bb406aa0451dea0e5106b0fb79a92b
Ryan Dewick <ryan.dewick@hermes-europe.co.uk>
2024-07-28 19:11:38 +0100

    GUI started

--- src/main/java/com/runemate/doublepatty/api/Utility.java
+++ src/main/java/com/runemate/doublepatty/api/Utility.java
@@ -4,6 +4,10 @@ import com.runemate.game.api.script.Execution;
 
 public class Utility {
 
+    public static int random(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
     public static void delay(int min, int max) {
         Execution.delay(min, max);
     }
